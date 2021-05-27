e9e7cc68aa47136d70b85a37f00586bf4c3232df
杜光磊 <18703937897@139.com>
2021-05-28 00:16:04 +0800

    优化部分代码啊

--- src/main/java/com/dgl/smodel/entity/BaseModel.java
+++ src/main/java/com/dgl/smodel/entity/BaseModel.java
@@ -1,8 +1,12 @@
 package com.dgl.smodel.entity;
 
 
+import com.dgl.common.utils.DGLDateUtils;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -22,11 +26,15 @@ public abstract class BaseModel implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer id;
 
+    @CreatedDate
     @Column(name = "create_time",columnDefinition="datetime not null COMMENT '创建时间'", nullable = false, updatable = false)
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DGLDateUtils.FORMAT_LONG)
     protected LocalDateTime createTime=LocalDateTime.now();
 
+    @LastModifiedDate
     @Column(name = "update_time",columnDefinition="timestamp default CURRENT_TIMESTAMP COMMENT '最后更新时间'")
+    @DateTimeFormat(pattern = DGLDateUtils.FORMAT_LONG)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DGLDateUtils.FORMAT_LONG)
     protected LocalDateTime updateTime;
 
     @Column(columnDefinition = "TINYINT(1) unsigned COMMENT '状态(0正常 1已删除)'", nullable = false)
